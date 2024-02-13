0dc5437d74c02a2583f0f59d27871eef57c818ec
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2024-02-13 15:28:55 +0500

    sadasd

--- src/Pages/PrinterTableData/Downloads.jsx
+++ src/Pages/PrinterTableData/Downloads.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import generatePDF, { Resolution, Margin } from "react-to-pdf";
 import jsPDF from "jspdf";
 
-const handlePrintTable2 = tableSelectedRows =>
+const handlePrintTable2 = (tableSelectedRows) =>
 {
   const doc = new jsPDF();
   const headerStyle = 'font-weight: bold; background:#3d41cf, color:white ;padding: 5px';
   const logsss = 'https://i.ibb.co/bPNS38G/Printer.png';
-  // const imageshowss = https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(tableSelectedRows[0].cardno)}&size=80x80;
-  const imageshowss = 0
-  const loadImage1 = new Promise(resolve =>
+  const imageshowss = 0;
+
+  const loadImage1 = new Promise((resolve) =>
   {
     const img1 = new Image();
-    img1.crossOrigin = "Anonymous";
+    img1.crossOrigin = 'Anonymous';
     img1.src = logsss;
     img1.onload = () =>
     {
@@ -20,11 +20,11 @@ const handlePrintTable2 = tableSelectedRows =>
     };
   });
 
-  const loadImage2 = new Promise(resolve =>
+  const loadImage2 = new Promise((resolve) =>
   {
     if (imageshowss) {
       const img2 = new Image();
-      img2.crossOrigin = "Anonymous";
+      img2.crossOrigin = 'Anonymous';
       img2.src = imageshowss;
       img2.onload = () =>
       {
@@ -37,61 +37,54 @@ const handlePrintTable2 = tableSelectedRows =>
 
   Promise.all([loadImage1, loadImage2]).then(([img1, img2]) =>
   {
+    doc.addImage(img1, 'JPEG', 0, 0, doc.internal.pageSize.getWidth(), doc.internal.pageSize.getHeight());
+
     const tableHtml = `
-            <div style="background: 'red'; background-size: cover; background-position: center; height: 100vh;">
-                <p style='position: absolute;
-                    top: 15%;
-                    left: 9%;
-                    font-size: 10px;
-                    color: #140d0d;'>
-                   1245
-                </p>
-                <p style='position: absolute;
-                  
-                    font-size: 16px;
-                    font-weight: bold;
-                    color: #140d0d;'>
-                    comment
-                </p>
-                <p style='position: absolute;
-                    top: 80%;
-                    left: 80%;
-                    font-size: 16px;
-                    font-weight: bold;
-                    color: #140d0d;'>
-                </p>
-            </div>
-        `;
+      <div>
+        <p style='position: absolute;
+          top: 15%;
+          left: 9%;
+          font-size: 10px;
+          color: #140d0d;'>
+          111111111111111
+        </p>
+        <p style='position: absolute;
+          font-size: 16px;
+          font-weight: bold;
+          color: red'>
+          2222222222222222222222 comment
+        </p>
+      </div>
+    `;
 
     const printContent = `
-            <html>
-                <head>
-                    <title>DataGrid Table</title>
-                    <style>
-                        @media print {
-                            body {
-                                padding: 0;
-                                margin: 0;
-                            }
-                            th {
-                                ${headerStyle}
-                            }
-                        }
-                    </style>
-                </head>
-                <body>${tableHtml}</body>
-            </html>
-        `;
+      <html>
+        <head>
+          <title>DataGrid Table</title>
+          <style>
+            @media print {
+              body {
+                padding: 0;
+                margin: 0;
+              }
+              th {
+                ${headerStyle}
+              }
+            }
+          </style>
+        </head>
+        <body>${tableHtml}</body>
+      </html>
+    `;
 
     doc.html(printContent, {
       callback: () =>
       {
         doc.save('table.pdf');
-      }
+      },
     });
   });
 };
-
 
 
 const Downloads = () =>
@@ -99,32 +92,7 @@ const Downloads = () =>
   return (
     <div>
       <button onClick={handlePrintTable2}>Download PDF</button>
-      <div id="container">
-        <div className="mt-5">
-          <div
-            style={{
-              margintop: "15% important",
-              marginLeft: "12% !important",
-              fontSize: "16px",
-              fontWeight: "bold",
-              color: "#140d0d",
-            }}
-          >
-            kasjfhdjksfh
-          </div>
-          <div
-            style={{
-              margintop: "15% important",
-              marginright: "13% !important",
-              fontSize: "16px",
-              fontWeight: "bold",
-              color: "red",
-            }}
-          >
-            Date
-          </div>
-        </div>
-      </div>
+
     </div>
   );
 };
