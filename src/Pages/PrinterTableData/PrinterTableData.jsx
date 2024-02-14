2b74782eeaddf8f544fcaf94a217956a61ce5f4a
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2024-02-14 10:23:47 +0500

    khdsa

--- src/Pages/PrinterTableData/PrinterTableData.jsx
+++ src/Pages/PrinterTableData/PrinterTableData.jsx
@@ -43,7 +43,7 @@ function PrinterTableData()
         getapi()
     }, [])
 
-   
+
 
     const handleRowClickInParent = item =>
     {
@@ -54,79 +54,86 @@ function PrinterTableData()
         }
     }
 
-const handlePrintTable2 = (tableSelectedRows) => {
-  const pdfWidth = 8.26; // in inches
-  const pdfHeight = 6.04; // in inches
+    const handlePrintTable2 = (tableSelectedRows) =>
+    {
+        const pdfWidth = 8.26; // in inches
+        const pdfHeight = 6.04; // in inches
 
-  // Create a new jsPDF instance with custom dimensions
-  const doc = new jsPDF({
-    orientation: "landscape",
-    unit: "in",
-    format: [pdfWidth, pdfHeight],
-  });
+        // Create a new jsPDF instance with custom dimensions
+        const doc = new jsPDF({
+            orientation: "landscape",
+            unit: "in",
+            format: [pdfWidth, pdfHeight],
+        });
 
-  const headerStyle =
-    "font-weight: bold; background:#3d41cf, color:white ;padding: 5px";
-  const logsss = "https://i.ibb.co/bPNS38G/Printer.png";
-  const imageshowss = 0;
+        const headerStyle =
+            "font-weight: bold; background:#3d41cf, color:white ;padding: 5px";
+        const logsss = "https://i.ibb.co/bPNS38G/Printer.png";
+        const imageshowss = 0;
 
-  const loadImage1 = new Promise((resolve) => {
-    const img1 = new Image();
-    img1.crossOrigin = "Anonymous";
-    img1.src = logsss;
-    img1.onload = () => {
-      resolve(img1);
-    };
-  });
+        const loadImage1 = new Promise((resolve) =>
+        {
+            const img1 = new Image();
+            img1.crossOrigin = "Anonymous";
+            img1.src = logsss;
+            img1.onload = () =>
+            {
+                resolve(img1);
+            };
+        });
 
-  const loadImage2 = new Promise((resolve) => {
-    if (imageshowss) {
-      const img2 = new Image();
-      img2.crossOrigin = "Anonymous";
-      img2.src = imageshowss;
-      img2.onload = () => {
-        resolve(img2);
-      };
-    } else {
-      resolve(null);
-    }
-  });
- const QRCodeCell = (props) => {
-   const url = `https://mirsal2newdubaitradeae.com/view/VehicleCard/${tableSelectedRows[0].cardno}`;
-   return <QRCode value={url} size={100} />;
- };
-  Promise.all([loadImage1, loadImage2]).then(([img1, img2]) => {
-    doc.addImage(
-      img1,
-      "JPEG",
-      0,
-      0,
-      doc.internal.pageSize.getWidth(),
-      doc.internal.pageSize.getHeight()
-    );
+        const loadImage2 = new Promise((resolve) =>
+        {
+            if (imageshowss) {
+                const img2 = new Image();
+                img2.crossOrigin = "Anonymous";
+                img2.src = imageshowss;
+                img2.onload = () =>
+                {
+                    resolve(img2);
+                };
+            } else {
+                resolve(null);
+            }
+        });
+        const QRCodeCell = (props) =>
+        {
+            const url = `https://mirsal2newdubaitradeae.com/view/VehicleCard/${tableSelectedRows[0].cardno}`;
+            return <QRCode value={url} size={100} />;
+        };
+        Promise.all([loadImage1, loadImage2]).then(([img1, img2]) =>
+        {
+            doc.addImage(
+                img1,
+                "JPEG",
+                0,
+                0,
+                doc.internal.pageSize.getWidth(),
+                doc.internal.pageSize.getHeight()
+            );
 
-    const tableHtml = `
+            const tableHtml = `
    <div style='position: relative'>
-        <p style="font-size: 0.15px;  margin-top:0.2px ; margin-left:0.5px; position: absolute">${tableSelectedRows[0].cardno}</p>
-        <p style="font-size: 0.15px;width:100px;display:flex margin-top:0.4px ; margin-left:6px; position: absolute ">${tableSelectedRows[0].Date}</p>
-         <p style="font-size: 0.15px;  margin-top:0.67px ; margin-left:2.5px; position: absolute">${tableSelectedRows[0].load}</p>
-        <p style="font-size: 0.15px;  margin-top:0.7px ; margin-left:5px; position: absolute ">${tableSelectedRows[0].vehicltype}</p>
-        <p style="font-size: 0.15px;  margin-top:1.3px ; margin-left:5px; position: absolute ">${tableSelectedRows[0].modelyear}</p>
-        <p style="font-size: 0.15px; font-weight:500; margin-top:1.8px ; margin-left:5px; position: absolute ">${tableSelectedRows[0].origin}</p>
-        <p style="font-size: 0.15px;  margin-top:2.3px ; margin-left:5px; position: absolute ">${tableSelectedRows[0].chassisno}</p>
-        <p style="font-size: 0.15px;  margin-top:2.8px ; margin-left:5px; position: absolute ">${tableSelectedRows[0].color}</p>
-        <p style="font-size: 0.15px;  margin-top:3.2px ; margin-left:5px; position: absolute ">${tableSelectedRows[0].enginno}</p>
-         <p style="font-size: 0.15px;  margin-top:4px ; width:5px; margin-left:4.3px; position: absolute ">${tableSelectedRows[0].comments}</p>
-          <p style="font-size: 0.15px;  margin-top:1.3px ; margin-left:2px; position: absolute ">${tableSelectedRows[0].enginehp}</p>
-           <p style="font-size: 0.15px;  margin-top:1.8px ; margin-left:2px; position: absolute ">${tableSelectedRows[0].weight}</p>
-           <p style="font-size: 0.15px;  margin-top:2.3px ; margin-left:1px; position: absolute ">${tableSelectedRows[0].importer_or_owner}</p>
-           <p style="font-size: 0.15px;  margin-top:2.8px ; margin-left:1px; position: absolute ">${tableSelectedRows[0].declearationno}</p>
-        <img style="height: 0.8px; width: 0.8px; margin-top: 5px; margin-left: 0.65px; position: absolute;" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJEAAACRCAAAAADmswX/AAABHElEQVR42u3aSRKDMAwEQP7/6eQD2IzwEki1rynkvqi0OMfnaecgIiIiIiIiIiIi+k/RcX2an41HISJaKmonw/XF9ShERFtF/Zv6t6dRiIieK2p+S0T0VlGaiUREDxKl3mbjtLljIyKaMdOWMmzLlE1ENGMP2c+mX2xGiYhu9UdnWdKvImmGERFtEDWjjmziJ1URIqJBUT9qqT8qzQVERGtFAeHs11J2EhFtFQUBg716sIQnItoqqs+0qWi88yciui+qbxDTXmjo7YiIaIaolGGlButuXSMimiYqvfeXco2I6A2iYKZNH5qIiB4kCrxrXkWJiBb0R2lRKZUSIqK1ovQ/WqX1zOptDRHRjD3kkkNERERERERERET0ftEXXunWZ07H080AAAAASUVORK5CYII=" alt="">
+        <p style="font-size: 0.15px;  margin-top:0.2px ; width:5px; margin-left:0.3px; position: absolute">${tableSelectedRows[0].cardno}</p>
+        <p style="font-size: 0.15px;width:100px;display:flex margin-top:0.1px ; margin-left:6.3px; position: absolute ">${tableSelectedRows[0].Date}</p>
+         <p style="font-size: 0.15px;  margin-top:0.67px ; width:5px; margin-left:0.3px; position: absolute">${tableSelectedRows[0].load}</p>
+        <p style="font-size: 0.15px;  margin-top:0.7px ;width:10px; margin-left:4.3px; position: absolute ">${tableSelectedRows[0].vehicltype}</p>
+        <p style="font-size: 0.15px;  margin-top:1.3px ;width:5px; margin-left:4.3px; position: absolute ">${tableSelectedRows[0].modelyear}</p>
+        <p style="font-size: 0.15px; font-weight:500;width:5px; margin-top:1.8px ; margin-left:4.3px; position: absolute ">${tableSelectedRows[0].origin}</p>
+        <p style="font-size: 0.15px;  margin-top:2.3px ;width:5px; margin-left:4.3px; position: absolute ">${tableSelectedRows[0].chassisno}</p>
+        <p style="font-size: 0.15px;  margin-top:2.8px ;width:5px; margin-left:4.3px; position: absolute ">${tableSelectedRows[0].color}</p>
+        <p style="font-size: 0.15px;  margin-top:3.2px ;width:5px; margin-left:4.3px; position: absolute ">${tableSelectedRows[0].enginno}</p>
+         <p style="font-size: 0.15px;  margin-top:4px ;width:5px; width:5px; margin-left:4.3px; position: absolute ">${tableSelectedRows[0].comments}</p>
+          <p style="font-size: 0.15px;  margin-top:1.3px ;width:5px; margin-left:0.3px; position: absolute ">${tableSelectedRows[0].enginehp}</p>
+           <p style="font-size: 0.15px;  margin-top:1.8px ;width:5px; margin-left:0.3px; position: absolute ">${tableSelectedRows[0].weight}</p>
+           <p style="font-size: 0.15px;  margin-top:2.3px ;width:10px; margin-left:0.3px; position: absolute ">${tableSelectedRows[0].importer_or_owner}</p>
+           <p style="font-size: 0.15px;  margin-top:2.8px ;width:5px; margin-left:0.3px; position: absolute ">${tableSelectedRows[0].declearationno}</p>
+        <img style="height: 0.8px; width: 0.8px; margin-top: 5px; margin-left: 0.5px; position: absolute;" src="data:image/png;base64,${tableSelectedRows[0].qrcode}" alt="">
 
       </div>
     `;
 
-    const printContent = `
+            const printContent = `
       <html>
         <head>
           <title>DataGrid Table</title>
@@ -146,13 +153,14 @@ const handlePrintTable2 = (tableSelectedRows) => {
       </html>
     `;
 
-    doc.html(printContent, {
-      callback: () => {
-        doc.save("table.pdf");
-      },
-    });
-  });
-};
+            doc.html(printContent, {
+                callback: () =>
+                {
+                    doc.save("table.pdf");
+                },
+            });
+        });
+    };
 
 
     const handleDownloadPDF = (tableSelectedRows) =>
