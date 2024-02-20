8deff844fb384c39f91017c46f4d6bb300441d5a
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2024-02-20 10:57:28 +0500

    dsfsdfsdf

--- src/Pages/PrinterTableData/PrinterTableData.jsx
+++ src/Pages/PrinterTableData/PrinterTableData.jsx
@@ -143,7 +143,7 @@ function PrinterTableData()
         <p style=" font-size: 0.15px;  margin-top:0.1px ; width:5px; margin-left:0.8px; position: absolute">${tableSelectedRows[0].cardno}</p>
         <p style="font-size: 0.15px;width:100px; margin-top:0.1px ; margin-left:6.6px; position: absolute ">${formatDate(tableSelectedRows[0].Date)}</p>
          <p style="font-size: 0.15px;  margin-top:0.55px ; width:5px; margin-left:3px; position: absolute">${tableSelectedRows[0].load}</p>
-        <p style=" font-size: 0.15px;  margin-top:0.5px ;width:10px; margin-left:4.3px; position: absolute ">${tableSelectedRows[0].VehicleBrandName} - ${tableSelectedRows[0].Vehiclemodel}(${tableSelectedRows[0].vehicltype})</p>
+        <p style=" font-size: 0.15px;  margin-top:0.5px ;width:2px; margin-left:4.3px; position: absolute ">${tableSelectedRows[0].VehicleBrandName} - ${tableSelectedRows[0].Vehiclemodel}(${tableSelectedRows[0].vehicltype})</p>
         <p style=" font-size: 0.15px;  margin-top:1.3px ;width:5px; margin-left:4.3px; position: absolute ">${tableSelectedRows[0].modelyear} - ${numberToWords(number)}</p>
         <p style=" font-size: 0.15px; font-weight:500;width:5px; margin-top:1.8px ; margin-left:4.3px; position: absolute ">${tableSelectedRows[0].origin}</p>
         <p style="font-size: 0.15px;  margin-top:2.3px ;width:5px; margin-left:4.3px; position: absolute ">${tableSelectedRows[0].chassisno}</p>
