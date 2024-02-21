6e59ce46224f59c3f47f54613b69052b8d5c91ae
zakirya592 <muhammadzakirya592@gmail.com>
2024-02-21 12:57:24 +0500

    updata

--- src/Datatablesource.jsx
+++ src/Datatablesource.jsx
@@ -59,6 +59,17 @@ export const ShipmentRequestColumns = [
     headerName: "VCC Generation Date",
     width: 200,
     headerClassName: "header-red",
+    valueFormatter: (params) => {
+      const dateStr = params.value;
+      if (dateStr) {
+        const date = new Date(dateStr);
+        const day = String(date.getDate()).padStart(2, "0");
+        const month = String(date.getMonth() + 1).padStart(2, "0");
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+      }
+      return "";
+    },
   },
   {
     field: "cardnos",
